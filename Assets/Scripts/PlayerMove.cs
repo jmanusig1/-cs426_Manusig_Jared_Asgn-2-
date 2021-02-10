82325def738036522933cd20eb95bcac961fb359
jmanusig1 <52005626+jmanusig1@users.noreply.github.com>
2021-02-09 22:45:19 -0600

    Added parkour section
    Added teh parkour section of the map (fast but hard)

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,13 +7,20 @@ public class PlayerMove : NetworkBehaviour
 {
     //variables that control the speed and rotation speed 
     public float speed = .1f;
-    public float rotationSpeed = .3f; 
+    public float rotationSpeed = .3f;
 
+    int jumps = 0;
+    public int maxJump = 1;
+    public float force = 300f; 
+
+    Rigidbody rb;
+    Transform tran;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        tran = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -24,16 +31,39 @@ public class PlayerMove : NetworkBehaviour
             return;
         }
 
+
         //variables for translation adn rotation 
         var r = Input.GetAxis("Horizontal") * rotationSpeed;
         var t = Input.GetAxis("Vertical") * speed;
 
         transform.Translate(0, 0, t);
         transform.Rotate(0, r, 0);
+
+        if (jumps < maxJump)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                jumps += 1;
+                rb.AddForce(tran.up * force);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            rb.AddForce(tran.up * 10f);
+        }
     }
 
     public override void OnStartLocalPlayer()
     {
         GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            jumps = 0;
+        }
     }
 }
